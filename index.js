9f54c59db30900969ebf8a2ba6c713d7d8a250ef
Jakub Was <wasjakub.jw@gmail.com>
2023-03-22 18:00:39 +0100

    feat: add body-parser

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const bodyParser = require('body-parser')
 const mongoConnect = require('./utils/database').mongoConnect
 
 dotenv.config()
 
 const app = express()
 const port = process.env.PORT || 3000
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
 const shopRoutes = require('./routes/shop')
 app.use('/shop', shopRoutes)
